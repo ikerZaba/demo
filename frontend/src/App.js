e1603656d47f7aa655aa28ebbcfba6150580f816
ikerZaba <ikerzabalegui@gmail.com>
2024-12-10 17:06:34 +0100

    Filtros programados correctamente

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-//import { Button,  Table } from 'reactstrap';
+
 
 class App extends Component {
   state = {
-    peliculas: []
+    peliculas: [],
+    sortBy: null,
+    sortOrder: 'asc'
   };
 
   async componentDidMount() {
@@ -11,6 +13,52 @@ class App extends Component {
     const body = await response.json();
     this.setState({peliculas: body});
   }
+
+  sortByTitulo = () => {
+    const {peliculas, sortOrder} = this.state;
+    const sortedPeliculas = peliculas.sort((a,b) =>{
+      if(sortOrder === 'asc') {
+        return a.titulo.localeCompare(b.titulo);
+      }else{
+        return b.titulo.localeCompare(a.titulo);
+      }
+    });
+
+    this.setState({
+      peliculas: sortedPeliculas,
+      sortBy: 'titulo'      
+    });
+
+    if(sortOrder === 'asc'){
+      this.setState({sortOrder : 'desc'});
+    }else{
+      this.setState({sortOrder : 'asc'});
+    }
+  };
+
+  sortByAño = () => {
+    const {peliculas, sortOrder} = this.state;
+    const sortedPeliculas = peliculas.sort((a,b) =>{
+      if(sortOrder === 'asc') {
+        return a.año - b.año;
+      }else{
+        return b.año - a.año;
+      }
+    });
+
+    this.setState({
+      peliculas: sortedPeliculas,
+      sortBy: 'año'      
+    });
+
+    if(sortOrder === 'asc'){
+      this.setState({sortOrder : 'desc'});
+    }else{
+      this.setState({sortOrder : 'asc'});
+    }
+  };
+
+
 
   render() {
     const {peliculas}= this.state;
@@ -28,9 +76,13 @@ class App extends Component {
               <table className="mt-4">
                 <thead>
                   <tr>
-                  <th width="30%">Título</th>
-                  <th width="10%">Año</th>
-                  <th width="60%">Trama</th>
+                  <th id="titulo" width="20%">Título
+                    <button id="boton_titulo" onClick={this.sortByTitulo}>
+                      {this.state.sortOrder === 'asc' ? '⬆️' : '⬇️'}</button></th>
+                  <th id="año" width="10%">Año
+                    <button id="boton_año" onClick={this.sortByAño}>
+                    {this.state.sortOrder === 'asc' ? '⬆️' : '⬇️'}</button></th>
+                  <th width="70%">Trama</th>
                 </tr>
                 </thead>                
                 <tbody>
